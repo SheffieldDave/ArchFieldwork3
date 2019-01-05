574ae03f114807ba1d86e8c4a8fa3c30dfd25f68
simon <simonstautner@live.com>
2019-01-05 14:21:19 +0100

    insert mainApp

--- app/src/main/java/org/wit/archfieldwork3/activities/SiteListActivity.kt
+++ app/src/main/java/org/wit/archfieldwork3/activities/SiteListActivity.kt
@@ -2,6 +2,7 @@ package org.wit.archfieldwork3.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import org.wit.archfieldwork3.R
 
 class SiteListActivity : AppCompatActivity() {
 
